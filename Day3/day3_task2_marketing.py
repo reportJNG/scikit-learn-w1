7939f1f7ebed36f4e9e849b2f527b637c22a6ac2
reportJNG <hamzaremali10@gmail.com>
2026-05-23 14:51:26 -0700

    getting started with the task 2

---
+++ Day3/day3_task2_marketing.py
@@ -0,0 +1,19 @@
+'''Create:
+
+day3_task2_marketing.py
+
+Your program must:
+
+1. Read CSV
+2. Print missing values before cleaning
+3. Fill missing numeric values using mean
+4. Print missing values after cleaning
+5. Convert city and plan using pd.get_dummies()
+6. Set X = all columns except bought
+7. Set y = bought
+8. Split train/test
+9. Train LogisticRegression
+10. Predict first test customer using X_test.iloc[[0]]
+11. Predict all X_test
+12. Calculate accuracy
+13. Print prediction + accuracy'''
