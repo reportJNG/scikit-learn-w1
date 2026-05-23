e1b28b311afe6fdc1b7b19cb01ccc2fe4a4a65d2
reportJNG <hamzaremali10@gmail.com>
2026-05-23 05:17:17 -0700

    first task day 3

---
+++ Day3/day3_task1_categories.py
@@ -0,0 +1,18 @@
+'''Your task
+
+Create:
+
+day3_task1_categories.py
+
+Your program must:
+
+1. Read CSV
+2. Print df before encoding
+3. Use pd.get_dummies() on city and plan
+4. Print df after encoding
+5. Set X = all columns except bought
+6. Set y = bought
+7. Split train/test
+8. Train LogisticRegression
+9. Predict one customer
+10. Calculate accuracy'''
