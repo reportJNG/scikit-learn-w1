d0da383cdd1924b0d3edc26d78654f9f7bca6f7a
reportJNG <hamzaremali10@gmail.com>
2026-05-23 15:31:10 -0700

    task 2 done

--- Day3/day3_task2_marketing.py
+++ Day3/day3_task2_marketing.py
@@ -17,3 +17,31 @@ Your program must:
 11. Predict all X_test
 12. Calculate accuracy
 13. Print prediction + accuracy'''
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+import pandas as panda
+
+data = panda.read_csv('./CSV/day3_task2_marketing.csv')
+#print(data.isnull().sum())
+calcarow= data.select_dtypes(include=['number']).columns
+#i dont think there is missing num in data but this is how u do fix data in pandas csv by calculation tfirst the row [i] 
+data[calcarow] = data[calcarow].fillna(data[calcarow].mean())
+data = panda.get_dummies(data,columns=['city','plan'],dtype=int)
+
+X=data.drop('bought',axis=1)
+y=data['bought']
+
+#model
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.3,random_state=42
+)
+
+model = LogisticRegression(max_iter=200)
+model.fit(X_train,y_train)
+predict1 = model.predict(X_test.iloc[[0]])
+predictall=model.predict(X_test)
+print(predictall)
+accurate=accuracy_score(y_test,predictall)
+print(accurate)
