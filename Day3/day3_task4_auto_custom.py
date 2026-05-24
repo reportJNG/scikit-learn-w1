a4fc4102a66a04d355df0a26478b49d3a1abb4eb
reportJNG <hamzaremali10@gmail.com>
2026-05-23 17:09:22 -0700

    this task 4 day 3 will be finished as tmr then revision for the houle days and rest just revesions

---
+++ Day3/day3_task4_auto_custom.py
@@ -0,0 +1,21 @@
+'''Your task ✅
+
+Create:
+
+day3_task4_auto_custom.py
+
+Your program must:
+
+1. Read CSV
+2. Fill missing numeric values
+3. Create custom customer as normal text data
+4. Combine original data + custom customer temporarily
+5. Encode city and plan using pd.get_dummies()
+6. Separate original data and custom customer again
+7. Set X and y
+8. Split train/test
+9. Train LogisticRegression
+10. Predict custom customer
+11. Predict X_test
+12. Calculate accuracy
+13. Print prediction + accuracy'''
